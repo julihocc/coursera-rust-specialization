81ee4b566e06c47733cdfbee54569d5f8a7cd184
julihocc <julihocc@yahoo.com>
2024-12-17 16:35:27 -0600

    Initialize demo_project with basic structure and a Hello World program

---
+++ rust-fundamentals/module-2/demo_project/target/debug/demo_project.d
@@ -0,0 +1,1 @@
+/home/julihocc/coursera-rust-specialization/rust-fundamentals/module-2/demo_project/target/debug/demo_project: /home/julihocc/coursera-rust-specialization/rust-fundamentals/module-2/demo_project/src/main.rs
