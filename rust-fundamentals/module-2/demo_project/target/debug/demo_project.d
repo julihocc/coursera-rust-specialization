09fa31665f592d8f71a897d8ba0d3d3172f9fa9a
julihocc <julihocc@yahoo.com>
2025-03-26 12:30:06 -0600

    Untrack target folder in demo_project

--- rust-fundamentals/module-2/demo_project/target/debug/demo_project.d
+++
@@ -1,1 +0,0 @@
-/home/julihocc/coursera-rust-specialization/rust-fundamentals/module-2/demo_project/target/debug/demo_project: /home/julihocc/coursera-rust-specialization/rust-fundamentals/module-2/demo_project/src/main.rs
--- rust-fundamentals/module-2/demo_project/target/debug/deps/demo_project-a52ec4568f975fd7.d
+++
@@ -1,5 +0,0 @@
-/home/julihocc/coursera-rust-specialization/rust-fundamentals/module-2/demo_project/target/debug/deps/demo_project-a52ec4568f975fd7: src/main.rs
-
-/home/julihocc/coursera-rust-specialization/rust-fundamentals/module-2/demo_project/target/debug/deps/demo_project-a52ec4568f975fd7.d: src/main.rs
-
-src/main.rs:
